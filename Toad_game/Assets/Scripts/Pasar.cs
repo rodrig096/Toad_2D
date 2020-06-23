ddd9f4dfb5281f0aa4c8300371520e97b98c054a
Alain <alencontrerascruz@gmail.com>
2020-06-23 01:57:07 -0400

    Transicion
    Transiciones bien bellacas

---
+++ Toad_game/Assets/Scripts/Pasar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pasar : MonoBehaviour
+{
+
+    public void Pasarescena(string pasarjuego)
+    {
+
+        SceneManager.LoadScene(3);
+
+    }
+
+}
